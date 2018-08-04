d22ae903e40bcaa97fb6ee16844e6cc2ac471a35
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-08-04 16:14:04 -0500

    FT now checks if we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,18 +19,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists...
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
         # She is immediately invited to enter a to-do item...
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
         # She types "Finish transfer function assignments" into a text box...
+        inputbox.send_keys('Finish transfer function assignments')
 
         # When she hits enter, the page updates, and now the page lists 
-        # "1: Finish transfer function assignments" as an item in a to-do list...
+        # "1: Finish transfer function assignments" as an item in a to-do list.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Finish transfer function assignments' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Shave Sammy and Clyde"...
-
+        self.fail('Finish your tests!!')
         # The page updates again, and now shows both items on her list...
 
         # Hanna wonders whether the site will remember her list. Then she sees
