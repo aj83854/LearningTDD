06fef647abc12ea34ce3a157e60e0bef32233665
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-08-12 23:10:21 -0500

    check the changes to functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Hanna has heard about a new to-do app.
         # She goes to check out it's homepage...
@@ -37,15 +42,18 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Finish transfer function assignments" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Finish transfer function assignments', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Finish transfer function assignments')
 
         # There is still a text box inviting her to add another item. She
         # enters "Shave Sammy and Clyde"...
-        self.fail('Finish your tests!!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Shave Sammy and Clyde')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         # The page updates again, and now shows both items on her list...
+        self.check_for_row_in_list_table('1: Finish transfer function assignments')
+        self.check_for_row_in_list_table('2: Shave Sammy and Clyde')
 
         # Hanna wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
